cb5deba0d85de884a5abbfa4c56ce62d4740df06
Livl <nottemt@gmail.com>
2015-05-19 16:13:06 +0200

    plansza + figurki

---
+++ test/chesscompiler/model/ChessBoard2Test.java
@@ -0,0 +1,38 @@
+package chesscompiler.model;
+
+import chesscompiler.model.pieces.Rook;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Liv
+ */
+public class ChessBoard2Test {
+    //ROOK TESTS
+    public ChessBoard2Test() {
+    }
+
+    @Test//pierwszy ruch bez wrogow
+    public void testGetValidMoves_BlackPawn_AloneFirstMove() {
+        ChessBoard board = new ChessBoard(8, 8);
+        board.addPiece("A8", new Rook(Rook.Color.BLACK));
+
+        String[] result = board.getValidMoves("A8");
+        
+        String[] expResult = {"A6", "A5","A1","A2","A3","A4","A7","B8","C8","D8","E8","F8","G8","H8"};
+        assertArrayEquals(expResult, result);
+    }
+    
+    @Test//pierwszy ruch bez wrogow
+    public void testGetValidMoves_WhitePawn_AloneFirstMove() {
+        ChessBoard board = new ChessBoard(8, 8);
+        board.addPiece("A1", new Rook(Rook.Color.BLACK));
+
+        String[] result = board.getValidMoves("A1");
+        
+        String[] expResult = {"A6", "A5","A8","A2","A3","A4","A7","B1","C1","D1","E1","F1","G1","H1"};
+        assertArrayEquals(expResult, result);
+    }
+    
+}
