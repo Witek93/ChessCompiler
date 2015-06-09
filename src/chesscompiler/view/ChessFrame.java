045616fca177611b1ab5ed034c303d5b97a66948
Livl <nottemt@gmail.com>
2015-06-09 17:39:45 +0200

    centering frame

--- src/chesscompiler/view/ChessFrame.java
+++ src/chesscompiler/view/ChessFrame.java
@@ -1,7 +1,9 @@
 package chesscompiler.view;
 
 import chesscompiler.model.Coordinates;
+import java.awt.Dimension;
 import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -38,6 +40,9 @@ public class ChessFrame extends JFrame {
         initMenuBar();
 
         add(this.board);
+        
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
     }
 
     private void initMenuBar() {
